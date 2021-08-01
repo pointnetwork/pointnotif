5b5399790464f54df9ec1e8d83bb2ce13cf3f5fd
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-08-01 14:45:05 -0700

    Initial work on detecting what events the user has subscribed to already.

--- content_scripts/detect_events.js
+++ content_scripts/detect_events.js
@@ -31,33 +31,47 @@ function extractRootDomainNoExt(url) {
 }
 
 function getContractName(hostname) {
-  let domainNoExt = extractRootDomainNoExt(hostname);
+  const domainNoExt = extractRootDomainNoExt(hostname);
   return domainNoExt.charAt(0).toUpperCase() + domainNoExt.slice(1);
 }
 
 async function getContract(contractName) {
   //TODO: What's the best way of getting the hostname here?
-  let contract = await fetch(`${window.location.origin}/v1/api/contract/load/${contractName}`)
+  const contract = await fetch(`${window.location.origin}/v1/api/contract/load/${contractName}`)
   return await contract.json();
 }
 
 async function getNodePublicKey() {
   //TODO: Is this the public key we want to filter notifications?
-  let publicKeyData = await fetch(`${window.location.origin}/v1/api/wallet/publicKey`)
-  let publicKey = await publicKeyData.json();
+  const publicKeyData = await fetch(`${window.location.origin}/v1/api/wallet/publicKey`)
+  const publicKey = await publicKeyData.json();
   return publicKey.data.publicKey;
+}
+
+async function getSubscriptionsByAddress(address) {
+  const subsData = await fetch(`${window.location.origin}/v1/api/subscription/${address}`)
+  const subs = await subsData.json();
+  return subs.data;
+}
+
+function isEventSubscribed(eventName, subs) {
+  return subs.some(sub => {
+    return sub.name == eventName
+  })
 }
 
 (async () => {
   // let point = window.wrappedJSObject.point;
   // let ping = await point.status.ping();
-  // console.log('omega255', point.status);
-  let hostname = window.location.hostname;
-  let contractName = getContractName(hostname);
-  let publicKey = await getNodePublicKey();
-  let contract = await getContract(contractName);
-  let abi = contract.data.abi;
-  let address = contract.data.address;
+  const hostname = window.location.hostname;
+  const contractName = getContractName(hostname);
+  const publicKey = await getNodePublicKey();
+  const contract = await getContract(contractName);
+  const abi = contract.data.abi;
+  const address = contract.data.address;
+  
+  const subs = await getSubscriptionsByAddress(address);
+
   let events = [];
   abi.forEach(elt => {
     if (elt.type == "event") {
@@ -70,10 +84,10 @@ async function getNodePublicKey() {
       subObj.options = {
 	filter: {to: publicKey},
       };
+      subObj.isSub = isEventSubscribed(elt.name, subs);
       events.push(subObj);
     }
   });
-  console.log('omega256', events)
   return events;
 }
 )();
