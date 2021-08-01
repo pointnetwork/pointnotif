03c0dcd3c96a6cd011efdc80691dc44dd23ef24c
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-08-01 14:45:26 -0700

    Removed garbage code.

--- content_scripts/subscribe.js
+++ content_scripts/subscribe.js
@@ -13,14 +13,6 @@ function handleError(error) {
   sending.then(handleResponse, handleError);
   let point = window.wrappedJSObject.point;
   let ping = await point.status.ping();
-  // let contractooo = await point.contract.load('Notifications');
-  console.log('omega2', events);
-  // await point.contract.call({contract: 'Notifications', method: 'notify'})
-
-  /* var xhr = new XMLHttpRequest();
-   * xhr.open("POST", `${window.location.origin}/v1/api/subscription/save`, true);
-   * xhr.setRequestHeader('Content-Type', 'application/json');
-   * xhr.send(JSON.stringify(events)); */
 
   let url = `${window.location.origin}/v1/api/subscription/save`;
   let resp = await fetch(url, {
@@ -31,7 +23,6 @@ function handleError(error) {
   let confirmedEvents = await resp.json();
 
   // point.contract.call({contract: 'Notifications', method: 'notify'})
-  console.log('omega4', confirmedEvents);
   return await confirmedEvents;
 }
 )();
